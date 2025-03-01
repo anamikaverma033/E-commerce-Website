6826523331ae2412d65a12647be822bb74f07be4
Anamika Verma <anamikaverma03388@gmail.com>
2025-03-01 12:58:24 +0530

    add Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -19,6 +19,7 @@ const Login = () => {
           : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
         }
       </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
     </form>
   )
 }
